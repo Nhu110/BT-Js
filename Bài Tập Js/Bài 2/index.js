3be76db271fe08129aaec3140f152668f89894f2
Nhu110 <152998841+Nhu110@users.noreply.github.com>
2024-01-09 20:17:02 +0700

    Add files via upload

---
+++ Bài Tập Js/Bài 2/index.js
@@ -0,0 +1,3 @@
+document.getElementById('demo1').innerHTML = 'Tài Liệu Học CSS';
+document.getElementById('demo2').innerHTML = 'Tài Liệu Học MySQL';
+document.getElementById('demo3').innerHTML = 'Taì Liệu Học PHP';
---
+++ Bài Tập Js/Bài 3/vidu.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 25;
+var c = a + b;
+
+document.write('c= ' + c);
+var x=  100 + 200;
+document.write('x= ' + x);
+
+var a = prompt("Nhập chiều dài HCN: ","0" );
+var b = prompt("Nhập chiều rộng HCN: ","0" );
+
+alert("Diện tích HCN là: " + a*b);
